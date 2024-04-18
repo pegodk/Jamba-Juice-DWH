7845c7da27d538c9827636261f2aba6cc8be9d86
Peter <32759685+pegodk@users.noreply.github.com>
2024-04-18 18:39:28 +0000

    Refactored gold table funcs

---
+++ spark_func_utils.py
@@ -0,0 +1,19 @@
+def generate_dim_table_references(source, target, timestamp_key, dim_table_refs, print_output=False):
+    
+    query_first = "SELECT s.*"
+    query_last = f"\nFROM {source} s"
+
+    for ref in dim_table_refs:
+        
+        # Construct first part of query: Selects
+        query_first += f""", COALESCE({ref["table_name"]}.{ref["surrogate_key"]}, 'N/A') AS {ref["surrogate_key"]} """
+
+        # Construct last part of query: Joins
+        query_last += f"""\nLEFT JOIN {ref["table_name"]} ON {ref["table_name"]}.{ref["join_key"]} = s.{ref["join_key"]}
+        AND s.{timestamp_key} BETWEEN {ref["table_name"]}.meta_valid_from AND {ref["table_name"]}.meta_valid_to"""
+
+    # Print output
+    if print_output:
+        print(query_first + query_last)
+
+    return query_first + query_last
